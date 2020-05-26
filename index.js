bdcd8ab070bdf3acfda30630271f88a5afda3f2c
Trevor Mearns <spiral272@gmail.com>
2020-05-26 12:57:04 +0900

    app is iterating through arrays in a way I can't understand

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ window.addEventListener('load', async () => {
 
     const getPeople = await fetch('https://swapi.dev/api/people/')
         .then((response) => response.json());
- 
+
 
     const getPlanets = await fetch('https://swapi.dev/api/planets/')
         .then((response) => response.json());
@@ -15,98 +15,64 @@ window.addEventListener('load', async () => {
     const getSpecies = await fetch('https://swapi.dev/api/species/')
         .then((response) => response.json());
 
-
     const people = getPeople.results;
     const planets = getPlanets.results;
     const species = getSpecies.results;
-console.log(people);
-
-    let characters = people.forEach((character) => {
-    // console.log('character name is ', character.name);
-    characterData = {
-    name: character.name,
-    birthyear: character.birth_year,
-    height: character.height,
-    mass: character.mass,
-   }
-    console.log(characterData);
-    return  characterData;
-})
+    console.log(people);
     
-    const planet =  planets.forEach((homeworld) => {
+    people.forEach((character) => {
+        // console.log('character name is ', character.name);
+        characterData = {
+            name: character.name,
+            birthyear: character.birth_year,
+            height: character.height,
+            mass: character.mass,
+        }
+        console.log(characterData);
+        return characterData;
+    })
+   
+    planets.forEach((homeworld) => {
         // console.log('characters homeworld is ', homeworld.name);
         homeWorld = homeworld.name;
         return homeWorld;
     })
 
-    const type = species.forEach((speciesType) => {
-    //     // console.log('species is ', speciesType.name);
+    species.forEach((speciesType) => {
+        //     // console.log('species is ', speciesType.name);
         classification = speciesType.name;
         return classification;
- })
+    });
 
- const data = [characters, planet, type];
-
- data.forEach(() => {
-    const tableRow = document.createElement('tr');
-    table.appendChild(tableRow);
-
-    const tableCell1 = document.createElement('td');
-    tableRow.appendChild(tableCell1);
-    const tableCell2 = document.createElement('td');
-    tableRow.appendChild(tableCell2);
-    const tableCell3 = document.createElement('td');
-    tableRow.appendChild(tableCell3);
-    const tableCell4 = document.createElement('td');
-    tableRow.appendChild(tableCell4);
-    const tableCell5 = document.createElement('td');
-    tableRow.appendChild(tableCell5);
-    const tableCell6 = document.createElement('td');
-    tableRow.appendChild(tableCell6);
-
-    tableCell1.textContent = characterData.name;
-    tableCell2.textContent = characterData.birthyear;
-    tableCell3.textContent = characterData.height;
-    tableCell4.textContent = characterData.mass;
-    tableCell5.textContent = homeWorld;
-    tableCell6.textContent = classification;
- })
     
-   
+    const data = [people, planets, species];
 
+    data.forEach((items) => {
+
+        items.forEach(() => {
+            const tableRow = document.createElement('tr');
+        table.appendChild(tableRow);
+
+        const tableCell1 = document.createElement('td');
+        tableRow.appendChild(tableCell1);
+        const tableCell2 = document.createElement('td');
+        tableRow.appendChild(tableCell2);
+        const tableCell3 = document.createElement('td');
+        tableRow.appendChild(tableCell3);
+        const tableCell4 = document.createElement('td');
+        tableRow.appendChild(tableCell4);
+        const tableCell5 = document.createElement('td');
+        tableRow.appendChild(tableCell5);
+        const tableCell6 = document.createElement('td');
+        tableRow.appendChild(tableCell6);
+
+        tableCell1.textContent = characterData.name;
+        tableCell2.textContent = characterData.birthyear;
+        tableCell3.textContent = characterData.height;
+        tableCell4.textContent = characterData.mass;
+        tableCell5.textContent = homeWorld;
+        tableCell6.textContent = classification;
+
+        }) 
+    })
 });
-
-
-
-
-
-
-
-
-
-
-
-
-        // fetch('https://swapi.dev/api/planets/'),
-        // fetch('https://swapi.dev/api/people/'),
-        // fetch('https://swapi.dev/api/species/')
-
-    //  fetch('https://swapi.dev/api/people/')
-    //  .then((response) => response.json())
-    //  .then((values) => {
-    //      const characters = values.results;
-    //      console.log('characters are ', characters );
-
-    //      characters.map((person) => {
-    //      const homeWorld = fetch(person.homeworld)
-    //        .then((response) => response.json())
-    //        .then((homeWorld) => {
-    //             // console.log('homeworld value is ', homeWorld)
-    //             const homeWorldName = homeWorld.name;
-    //            console.log( person.name + "is from " + homeWorldName);
-    //        })
-    //      })
-    //   })
-    //   .catch (error => console.log('you got yerself an error ', error))
-
-
