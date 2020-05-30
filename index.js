fac2f32b4f29687f458fdff483bed0bf3736c73e
Trevor Mearns <spiral272@gmail.com>
2020-05-30 13:50:55 +0900

    consts world and species returning undefined. STUCK

--- index.js
+++ index.js
@@ -39,23 +39,44 @@ window.addEventListener('load', async () => {
             const species = speciesData.forEach((species) => {
                 console.log('species name = ', species.name);
                 return species.name;
-            })
+            });
 
                 const tableData = {
                         name: character.name,
                         height: character.height,
                         mass: character.mass,
-                        homeworld: world,
+                        homeWorld: world,
                         species: species,
                         birthDate: character.birth_year
                     };
 
                     return tableData;
         };
-        getCharacterData(character).then((data) => {
-            console.log(data);
-
-        })
+        getCharacterData(character).then((tableData) => {
+            console.log(tableData);
+            const tableRow = document.createElement('tr');
+            table.appendChild(tableRow);
+    
+            const tableCell1 = document.createElement('td');
+            tableRow.appendChild(tableCell1);
+            const tableCell2 = document.createElement('td');
+            tableRow.appendChild(tableCell2);
+            const tableCell3 = document.createElement('td');
+            tableRow.appendChild(tableCell3);
+            const tableCell4 = document.createElement('td');
+            tableRow.appendChild(tableCell4);
+            const tableCell5 = document.createElement('td');
+            tableRow.appendChild(tableCell5);
+            const tableCell6 = document.createElement('td');
+            tableRow.appendChild(tableCell6);
+    
+            tableCell1.textContent = tableData.name;
+            tableCell2.textContent = tableData.birthDate;
+            tableCell3.textContent = tableData.height;
+            tableCell4.textContent = tableData.mass;
+            tableCell5.textContent = tableData.homeWorld;
+            tableCell6.textContent = tableData.species;
+        });
         
         
        
