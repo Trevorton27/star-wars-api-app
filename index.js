0d82fb2491e5f270eae4300bfd734180affd38aa
Trevor Mearns <Trevor Mearns>
2020-07-31 15:04:26 +0900

    pre refactor returvalue() and submit button event

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const tableDisplay = document.getElementById("table-display");
 const table = document.getElementById("main-table");
 const inputField = document.getElementById("input-field");
 const searchResult = document.getElementById("search-results");
@@ -24,29 +25,6 @@ window.addEventListener('load', async () => {
     console.log('people api results: ', getPeople.results);
     const people = getPeople.results;
 
-    const returnValue = async function () {
-        await fetch('https://swapi.dev/api/people/?search=' + inputField.value)
-            .then(response => response.json())
-            .then((response) => {
-                const world = async () => await getHomeWorld(response.homeworld);
-                const species =  async () => await getSpecies(response.species);
-                const searchedCharacter = response.results;
-
-                const searchReturnData = {
-                    name: searchedCharacter.name,
-                    height: searchedCharacter.height,
-                    mass: searchedCharacter.mass,
-                    homeWorld: world,
-                    species: species,
-                    birthDate: searchedCharacter.birth_year
-                }
-                console.log('response is: ', searchedCharacter);
-                // console.log('search return data is: ', searchReturnData);
-                return searchReturnData;
-            })
-        }
-
-
     people.forEach((character) => {
 
         const getHomeWorld = async (homeWorld) => {
@@ -61,6 +39,7 @@ window.addEventListener('load', async () => {
         };
 
        
+
         const getCharacterData = async (character) => {
             console.log(typeof character);
             const world = await getHomeWorld(character.homeworld);
@@ -102,100 +81,102 @@ window.addEventListener('load', async () => {
             tableCell5.textContent = tableData.homeWorld;
             tableCell6.textContent = tableData.species;
         });
-
-   
-    })
-    document
-    .getElementById("submitButton")
-    .addEventListener('click', () => {
-        console.log('Im working dawg!');
-        returnValue();  
-        // console.log(returnValue());      // if (inputField.value !== '') {
-        //     if (clicked === true) searchResult.textContent = '';
-        //     returnValue();
-        //     inputField.value = "";
-        //     inputField.focus;
-        // } else {
-        //     alert('Please enter a character name. May the force be with you.');
-        // }
     })
 });
 
-// const returnValue = async function () {
-//     return await fetch('https://swapi.dev/api/people/?search=' + inputField.value)
-//         .then(response => response.json())
-//         .then(response => {
-//             // console.log( response.results);
-//             const data = response.results;
-//             return data;
-//         })
-//         .then((data) => {
-//             function renderTableHeader() {
+const returnValue = async function () {
+    return await fetch('https://swapi.dev/api/people/?search=' + inputField.value)
+        .then(response => response.json())
+        .then(response => {
+            // console.log( response.results);
+            const data = response.results;
+            return data;
+        })
+        .then((data) => {
 
-//                 const tableHead1 = document.createElement('th');
-//                 searchTable.appendChild(tableHead1);
-//                 const tableHead2 = document.createElement('th');
-//                 searchTable.appendChild(tableHead2);
-//                 const tableHead3 = document.createElement('th');
-//                 searchTable.appendChild(tableHead3);
-//                 const tableHead4 = document.createElement('th');
-//                 searchTable.appendChild(tableHead4);
-//                 const tableHead5 = document.createElement('th');
-//                 searchTable.appendChild(tableHead5);
-//                 const tableHead6 = document.createElement('th');
-//                 searchTable.appendChild(tableHead6);
+            console.log("data is ", data);
+            function renderTableHeader() {
 
-//                 tableHead1.textContent = 'Name';
-//                 tableHead2.textContent = 'Height';
-//                 tableHead3.textContent = 'Mass';
-//                 tableHead4.textContent = 'Hair Color';
-//                 tableHead5.textContent =  'Homeworld';
-//                 tableHead6.textContent = 'Species';
+                const tableHead1 = document.createElement('th');
+                searchTable.appendChild(tableHead1);
+                const tableHead2 = document.createElement('th');
+                searchTable.appendChild(tableHead2);
+                const tableHead3 = document.createElement('th');
+                searchTable.appendChild(tableHead3);
+                const tableHead4 = document.createElement('th');
+                searchTable.appendChild(tableHead4);
+                const tableHead5 = document.createElement('th');
+                searchTable.appendChild(tableHead5);
+                const tableHead6 = document.createElement('th');
+                searchTable.appendChild(tableHead6);
 
+                tableHead1.textContent = 'Name';
+                tableHead2.textContent = 'Height';
+                tableHead3.textContent = 'Mass';
+                tableHead4.textContent = 'Hair Color';
+                tableHead5.textContent =  'Homeworld';
+                tableHead6.textContent = 'Species';
 
-//                 rendered = true;
-//             }
-//             if (rendered === false) {
-//                 renderTableHeader();
-//             }
-//             console.log(data);
+                tableDisplay.remove();
+                table.remove();
+                rendered = true;
+            }
+            if (rendered === false) {
+                renderTableHeader();
+            }
+            console.log(data);
          
-//             data.forEach((data) => {
-//                 const species =   getSpecies(data.species)
-//                 const searchReturnData = {
-//                     name: data.name,
-//                     height: data.height,
-//                     mass: data.mass,
-//                     hairColor: data.hair_color,
-//                     species: species 
-//                 }
+            data.forEach((data) => {
+                const species =   getSpecies(data.species)
+                const searchReturnData = {
+                    name: data.name,
+                    height: data.height,
+                    mass: data.mass,
+                    hairColor: data.hair_color,
+                    species: species 
+                }
 
-//                 const tableRow = document.createElement('tr');
-//                 searchResult.appendChild(tableRow);
+                console.log("getSpecies() is returning ", getSpecies());
 
-//                 const tableCell1 = document.createElement('td');
-//                 tableRow.appendChild(tableCell1);
-//                 const tableCell2 = document.createElement('td');
-//                 tableRow.appendChild(tableCell2);
-//                 const tableCell3 = document.createElement('td');
-//                 tableRow.appendChild(tableCell3);
-//                 const tableCell4 = document.createElement('td');
-//                 tableRow.appendChild(tableCell4);
-//                 const tableCell5 = document.createElement('td');
-//                 tableRow.appendChild(tableCell5);
-//                 const tableCell6 = document.createElement('td');
-//                 tableRow.appendChild(tableCell6);
+                const tableRow = document.createElement('tr');
+                searchResult.appendChild(tableRow);
 
-//                 tableCell1.textContent = searchReturnData.name;
-//                 tableCell2.textContent = searchReturnData.height;
-//                 tableCell3.textContent = searchReturnData.mass;
-//                 tableCell4.textContent = searchReturnData.hairColor;
-//                 tableCell5.textContent = 'Hey you guys!';
-//                 tableCell6.textContent = searchReturnData.species;
-//                 console.log('search return data: ', searchReturnData);
+                const tableCell1 = document.createElement('td');
+                tableRow.appendChild(tableCell1);
+                const tableCell2 = document.createElement('td');
+                tableRow.appendChild(tableCell2);
+                const tableCell3 = document.createElement('td');
+                tableRow.appendChild(tableCell3);
+                const tableCell4 = document.createElement('td');
+                tableRow.appendChild(tableCell4);
+                const tableCell5 = document.createElement('td');
+                tableRow.appendChild(tableCell5);
+                const tableCell6 = document.createElement('td');
+                tableRow.appendChild(tableCell6);
 
-//                 clicked = true;
-//             });
-//         })
-// }
+                tableCell1.textContent = searchReturnData.name;
+                tableCell2.textContent = searchReturnData.height;
+                tableCell3.textContent = searchReturnData.mass;
+                tableCell4.textContent = searchReturnData.hairColor;
+                tableCell5.textContent = 'Hey you guys!';
+                tableCell6.textContent = searchReturnData.species;
+                console.log('search return data: ', searchReturnData);
 
+                clicked = true;
+            });
+        })
+}
+
+document
+    .getElementById("submitButton")
+    .addEventListener('click', () => {
+
+        if (inputField.value !== '') {
+            if (clicked === true) searchResult.textContent = '';
+            returnValue();
+            inputField.value = "";
+            inputField.focus;
+        } else {
+            alert('Please enter a character name. May the force be with you.');
+        }
+    })
